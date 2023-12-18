5f83edba6fba3add464541d31ad48c2b92aa8c84
Taha ÖZMEN <139132956+tahaozmn@users.noreply.github.com>
2023-12-18 10:40:11 +0300

    Add files via upload

---
+++ SwiftTesting/MathematicFunctions.swift
@@ -0,0 +1,24 @@
+//
+//  MathematicFunctions.swift
+//  SwiftTesting
+//
+//  Created by Taha Özmen on 18.12.2023.
+//
+
+import Foundation
+
+class MathematicFunctions {
+    
+    func addIntegers(x: Int, y:Int) -> Int {
+        return x + y
+    }
+    
+    func multiplyNumbers(x: Int, y:Int) -> Int {
+        return x * y
+    }
+    
+    func divideIntegers(x: Int, y:Int) -> Int {
+        return x / y
+    }
+    
+}
